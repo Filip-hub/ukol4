186ca11749f740a0577c035ab78e413d1fcd6ed7
Filip Votek <f.vojtek01@gmail.com>
2020-12-16 16:45:01 +0100

    ukol4

---
+++ ukol 4/ukol 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ukol_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej rok:");
+            int rok = int.Parse(Console.ReadLine());
+
+            if (DateTime.IsLeapYear(rok))
+            {
+                Console.WriteLine("Rok je prestupny");
+            }
+            else
+            {
+                Console.WriteLine("Rok neni prestupny");
+            }
+            Console.ReadKey();
+        }
+    }
+}
